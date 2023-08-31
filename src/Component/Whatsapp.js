bc6e64351349bc9109f7de60bec9c0558196ae0f
Brahmasura <ashlincds@gmail.com>
2023-09-01 01:45:22 +0530

    the university overflowing content adjusted and whatsapp logo added

---
+++ src/Component/Whatsapp.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import style from './whatsapp.module.scss';
+import icon from '../Assets/Whatsapp/whatsappIcon.svg';
+
+const Whatsapp = () => {
+
+   const openWhatsapp = () => {
+        window.open('https://api.whatsapp.com/send?phone=9760390807', '_blank');
+   }
+  return (
+   <>
+    <div className={style.iconDiv} onClick={openWhatsapp}>
+        <img src={icon} alt="whatsapp" />
+        <p>Contact Us</p>
+    </div>
+   </>
+  )
+}
+
+export default Whatsapp;
--- src/Layouts/BasicLayout/BasicLayout.js
+++ src/Layouts/BasicLayout/BasicLayout.js
@@ -2,12 +2,14 @@ import React from 'react';
 import layout from './basicLayout.module.scss';
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
+import Whatsapp from '../../Component/Whatsapp';
 
 const BasicLayout = ({children}) => {
   return (
     <div className={layout.scroller}>
     <Header/>
     <div>{children}</div>
+    <Whatsapp/>
     <Footer/>
     </div>
   )
