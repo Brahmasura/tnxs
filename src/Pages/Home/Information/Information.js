6ff6e87f112a2df4fde252ed105f648494ce6d2f
Brahmasura <ashlincds@gmail.com>
2023-09-05 18:24:42 +0530

    the info section links are set and also images adjusted

--- src/Pages/Home/Information/Information.js
+++ src/Pages/Home/Information/Information.js
@@ -1,22 +1,34 @@
-import React from "react";
+import React, { useContext } from "react";
 import info from "./information.module.scss";
 import { NavLink } from "react-router-dom";
+import { LinkContext } from "../../../App";
 // import infoOne from '../../../Assets/Information/infoOne.jpg';
 // import infoTwo from '../../../Assets/Information/infoTwo.jpg';
 // import infoThree from '../../../Assets/Information/infoThree.jpg';
 // import infoFour from '../../../Assets/Information/infoFour.jpg';
 
 const Information = () => {
+  const { handleLinkClick } = useContext(LinkContext);
+
   return (
     <>
       <div className={`container-fluid ${info.infoContainer}`}>
         <div className="row">
           <div className={`col-md-6 col-lg-6 ${info.leftCol}`}>
-            <h1 className={info.upperText}>LIFE IN RUSSIA</h1>
+            <NavLink
+              to={"/lifeInRussia"}
+              onClick={() => {
+                handleLinkClick(3);
+                window.scrollTo(0, 0);
+              }}
+            >
+              <h1 className={info.upperText}>LIFE IN RUSSIA</h1>
+            </NavLink>
             <hr className={info.hrStyle} />
             <NavLink
               to={"/accomodation"}
               onClick={() => {
+                handleLinkClick(3);
                 window.scrollTo(0, 0);
               }}
             >
@@ -26,11 +38,19 @@ const Information = () => {
           <div className={`col-md-6 col-lg-6 ${info.rightCol}`}>
             <div className="row">
               <div className={`col-md-12 col-lg-12 ${info.rightTopCol}`}>
-                <h1 className={info.upperText}>WHY RUSSIA</h1>
+                <NavLink
+                  to={"/whyRussia"}
+                  onClick={() => {
+                    handleLinkClick(1);
+                    window.scrollTo(0, 0);
+                  }}
+                >
+                  <h1 className={info.upperText}>WHY RUSSIA</h1>
+                </NavLink>
                 <hr className={info.hrStyle} />
                 <NavLink
                   to={"/advantages"}
-                  onClick={() => window.scrollTo(0, 0)}
+                  onClick={() => {handleLinkClick(1); window.scrollTo(0, 0)}}
                 >
                   <h1>Advantages of education in Russia </h1>
                 </NavLink>
@@ -38,23 +58,39 @@ const Information = () => {
               <div className={`col-md-12 col-lg-12 ${info.rightBottomCol}`}>
                 <div className="row">
                   <div className={`col-md-6 col-lg-6 ${info.rightBotLeft}`}>
-                    <h1 className={info.upperText}>LIFE IN RUSSIA</h1>
+                    <NavLink
+                      to={"/lifeInRussia"}
+                      onClick={() => {
+                        handleLinkClick(3);
+                        window.scrollTo(0, 0);
+                      }}
+                    >
+                      <h1 className={info.upperText}>LIFE IN RUSSIA</h1>
+                    </NavLink>
                     <hr className={info.hrStyle} />
                     <NavLink
                       to={"/prices"}
-                      onClick={() => window.scrollTo(0, 0)}
+                      onClick={() => {handleLinkClick(3); window.scrollTo(0, 0)}}
                     >
                       <h1>Prices in Russia</h1>
                     </NavLink>
                   </div>
                   <div className={`col-md-6 col-lg-6 ${info.rightBotRight}`}>
-                    <h1 className={info.upperText}>WHY RUSSIA</h1>
+                    <NavLink
+                      to={"/studyInRussia"}
+                      onClick={() => {
+                        handleLinkClick(2);
+                        window.scrollTo(0, 0);
+                      }}
+                    >
+                      <h1 className={info.upperText}>STUDY IN RUSSIA</h1>
+                    </NavLink>
                     <hr className={info.hrStyle} />
                     <NavLink
                       to={"/ranking"}
                       onClick={() => window.scrollTo(0, 0)}
                     >
-                      <h1>Russian Universities in World Ranking</h1>
+                      <h1>Tution fee in Russia</h1>
                     </NavLink>
                   </div>
                 </div>
