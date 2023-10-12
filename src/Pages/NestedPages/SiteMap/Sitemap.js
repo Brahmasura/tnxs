8c830152fc1715f06b4df07ed22ef59096d9da75
Brahmasura <ashlincds@gmail.com>
2023-10-12 18:25:06 +0530

    the sitemap lnk redirection has been updated

--- src/Pages/NestedPages/SiteMap/Sitemap.js
+++ src/Pages/NestedPages/SiteMap/Sitemap.js
@@ -17,47 +17,47 @@ const Sitemap = () => {
 
         <ul>
           <li>
-          <NavLink to={"/whyRussia"} onClick={() => window.scrollTo(0,0)}>
+          <NavLink to={"/whyRussia"} onClick={() => {handleLinkClick(1); window.scrollTo(0,0);}}>
             Why Russia
             </NavLink>
             <ul>
-              <li><NavLink to={"/advantages"} onClick={() => window.scrollTo(0,0)}>Advantages </NavLink></li>
-              <li><NavLink to={"/advantages"} onClick={() => window.scrollTo(0,0)}>Work Opportunities </NavLink></li>
-              <li><NavLink to={"/advantages"} onClick={() => window.scrollTo(0,0)}>Testimonial </NavLink></li>
-              <li><NavLink to={"/ranking"} onClick={() => window.scrollTo(0,0)}>World Ranking </NavLink></li>
+              <li><NavLink to={"/advantages"} onClick={() => {handleLinkClick(1); window.scrollTo(0,0);}}>Advantages </NavLink></li>
+              <li><NavLink to={"/advantages"} onClick={() => {handleLinkClick(1); window.scrollTo(0,0);}}>Work Opportunities </NavLink></li>
+              <li><NavLink to={"/whyRussTesti"} onClick={() => {handleLinkClick(1); window.scrollTo(0,0);}}>Testimonial </NavLink></li>
+              <li><NavLink to={"/ranking"} onClick={() => {handleLinkClick(1); window.scrollTo(0,0);}}>World Ranking </NavLink></li>
             </ul>
           </li><br/>
 
           <li>
-          <NavLink to={"/studyInRussia"} onClick={() => window.scrollTo(0,0)}>Study In Russia </NavLink> 
+          <NavLink to={"/studyInRussia"} onClick={() => {handleLinkClick(2); window.scrollTo(0,0);}}>Study In Russia </NavLink> 
             <ul>
-              <li><NavLink to={"/mbbsInRussia" } onClick={() => window.scrollTo(0,0)}>MBBS In Russia </NavLink></li>
-              <li><NavLink to={"/studyInEng"} onClick={() => window.scrollTo(0,0)}>Study In English </NavLink></li>
-              <li><NavLink to={"/preparatory"} onClick={() => window.scrollTo(0,0)}>Preparatory Department </NavLink></li>
-              <li><NavLink to={"/universities"} onClick={() => window.scrollTo(0,0)}>Top Univesities </NavLink></li>
-              <li><NavLink to={"/degreeValidation"} onClick={() => window.scrollTo(0,0)}>Validation of Degree </NavLink></li>
-              <li><NavLink to={"/tuitionFee"} onClick={() => window.scrollTo(0,0)}>Tuition Fee </NavLink></li>
+              <li><NavLink to={"/mbbsInRussia" } onClick={() => {handleLinkClick(2); window.scrollTo(0,0);}}>MBBS In Russia </NavLink></li>
+              <li><NavLink to={"/studyInEng"} onClick={() => {handleLinkClick(2); window.scrollTo(0,0);}}>Study In English </NavLink></li>
+              <li><NavLink to={"/preparatory"} onClick={() => {handleLinkClick(2); window.scrollTo(0,0);}}>Preparatory Department </NavLink></li>
+              <li><NavLink to={"/studyUni"} onClick={() => {handleLinkClick(2); window.scrollTo(0,0);}}>Top Univesities </NavLink></li>
+              <li><NavLink to={"/degreeValidation"} onClick={() => {handleLinkClick(2); window.scrollTo(0,0);}}>Validation of Degree </NavLink></li>
+              <li><NavLink to={"/tuitionFee"} onClick={() => {handleLinkClick(2); window.scrollTo(0,0);}}>Tuition Fee </NavLink></li>
             </ul>
           </li><br/>
 
           <li>
-          <NavLink to={"/lifeInRussia"} onClick={() => window.scrollTo(0,0)}>Life In Russia</NavLink>
+          <NavLink to={"/lifeInRussia"} onClick={() => {handleLinkClick(3); window.scrollTo(0,0);}}>Life In Russia</NavLink>
             <ul>
-              <li><NavLink to={"/prices"} onClick={() => window.scrollTo(0,0)}>Prices </NavLink></li>
-              <li><NavLink to={"/accomodation"} onClick={() => window.scrollTo(0,0)}>Accomodation </NavLink></li>
-              <li><NavLink to={"/climate"} onClick={() => window.scrollTo(0,0)}>Climate </NavLink></li>
-              <li><NavLink to={"/transport"} onClick={() => window.scrollTo(0,0)}>Transport </NavLink> </li>
-              <li><NavLink to={"/safety"} onClick={() => window.scrollTo(0,0)}>Safety </NavLink></li> 
-              <li><NavLink to={"/healthcare"} onClick={() => window.scrollTo(0,0)}>Healthcare </NavLink></li>
+              <li><NavLink to={"/prices"} onClick={() => {handleLinkClick(3); window.scrollTo(0,0);}}>Prices </NavLink></li>
+              <li><NavLink to={"/accomodation"} onClick={() => {handleLinkClick(3); window.scrollTo(0,0);}}>Accomodation </NavLink></li>
+              <li><NavLink to={"/climate"} onClick={() => {handleLinkClick(3); window.scrollTo(0,0);}}>Climate </NavLink></li>
+              <li><NavLink to={"/transport"} onClick={() => {handleLinkClick(3); window.scrollTo(0,0);}}>Transport </NavLink> </li>
+              <li><NavLink to={"/safety"} onClick={() => {handleLinkClick(3); window.scrollTo(0,0);}}>Safety </NavLink></li> 
+              <li><NavLink to={"/healthcare"} onClick={() => {handleLinkClick(3); window.scrollTo(0,0);}}>Healthcare </NavLink></li>
             </ul>
           </li><br/>
 
-            <li><NavLink to={"/mbbsRuss"} onClick={() => window.scrollTo(0,0)}>MBBS Abroad </NavLink>
+            <li><NavLink to={"/mbbsRuss"} onClick={() => {handleLinkClick(0); window.scrollTo(0,0);}}>MBBS Abroad </NavLink>
             <ul>
-            <li><NavLink to={"/mbbsRuss"} onClick={() => window.scrollTo(0,0)}>MBBS In Russia</NavLink></li>
-            <li><NavLink to={"/mbbsKazak"} onClick={() => window.scrollTo(0,0)}>MBBS In Kazakhstan</NavLink></li>
-            <li><NavLink to={"/mbbsUzbek"} onClick={() => window.scrollTo(0,0)}>MBBS In Uzbekistan</NavLink></li>
-            <li><NavLink to={"/mbbsItaly"} onClick={() => window.scrollTo(0,0)}>MBBS In Italy</NavLink></li>
+            <li><NavLink to={"/mbbsRuss"} onClick={() => {handleLinkClick(0); window.scrollTo(0,0);}}>MBBS In Russia</NavLink></li>
+            <li><NavLink to={"/mbbsKazak"} onClick={() => {handleLinkClick(0); window.scrollTo(0,0);}}>MBBS In Kazakhstan</NavLink></li>
+            <li><NavLink to={"/mbbsUzbek"} onClick={() => {handleLinkClick(0); window.scrollTo(0,0);}}>MBBS In Uzbekistan</NavLink></li>
+            <li><NavLink to={"/mbbsItaly"} onClick={() => {handleLinkClick(0); window.scrollTo(0,0);}}>MBBS In Italy</NavLink></li>
             </ul>
             </li>
          
